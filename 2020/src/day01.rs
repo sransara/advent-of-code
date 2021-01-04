41c49c245afe6180db694a686f7ec7de34478d7b
sransara <git@sransara.com>
2021-01-04 15:33:29 -0800

    day 1 part 1

--- 2020/src/day01.rs
+++ 2020/src/day01.rs
@@ -1,4 +1,19 @@
+use std::collections::HashSet;
 
-pub fn part1(inp: String) {
-    println!("{}", inp.chars().rev().collect::<String>());
+pub fn part1(input: String) {
+    let nums: Vec<_> = input.lines().map(|line: &str| line.parse::<i32>().unwrap()).collect();
+    let (x, y) = two_sum(nums, 2020);
+    println!("{}", x * y);
+}
+
+fn two_sum(nums: Vec<i32>, target: i32) -> (i32, i32) {
+    let mut seen = HashSet::new();
+    for num in nums {
+        if seen.contains(&num) {
+            return (target - num, num);
+        }
+        seen.insert(target - num);
+    }
+
+    panic!("No solution found");
 }
