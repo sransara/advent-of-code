a5e432d56ef79354df751498fc50fc378c4a110f
sransara <git@sransara.com>
2021-01-11 15:28:08 -0800

    day 1 imaginary part k

--- 2020/src/day01.rs
+++ 2020/src/day01.rs
@@ -41,3 +41,49 @@ fn three_sum(nums: Vec<i32>, target: i32) -> (i32, i32, i32) {
 
     panic!("No solution found");
 }
+
+#[allow(dead_code)]
+pub fn partk(input: String) {
+    let nums: Vec<_> = input.lines().map(|line: &str| line.parse::<i32>().unwrap()).collect();
+    let kset = k_sum(3, nums, 2020);
+    println!("{}", kset.iter().fold(1, |a, b| a * b));
+}
+
+fn k_sets(k: i32, nums: &Vec<i32>) -> Vec<HashSet<i32>> {
+    if k == 1 {
+        return nums.iter().map(|&x| [x].iter().cloned().collect()).collect();
+    }
+
+    let smalls = k_sets(k - 1, nums);
+    let mut result: Vec<_> = Vec::new();
+
+    for num in nums.iter().cloned() {
+        for small in &smalls {
+            let mut item: HashSet<i32> = small.iter().cloned().collect();
+            if item.insert(num) {
+                result.push(item);
+            }
+        }
+    }
+
+    return result;
+}
+
+fn k_sum(k: i32, nums: Vec<i32>, target: i32) -> Vec<i32> {
+    // make all k-1 sets from nums
+    let lhss = k_sets(k - 1, &nums);
+
+    // check if each sets sum + z == target
+    let available: HashSet<_> = nums.iter().cloned().collect();
+    for lhs in lhss {
+        let y: i32 = lhs.iter().sum();
+        let z = target - y;
+        if !lhs.contains(&z) && available.contains(&z) {
+            let mut result = lhs;
+            result.insert(z);
+            return result.iter().cloned().collect();
+        }
+    }
+    
+    panic!("No solution found");
+}
