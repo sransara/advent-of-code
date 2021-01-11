7b86a122fa787789f5e93d4dfc5776995030a5ec
sransara <git@sransara.com>
2021-01-10 17:17:02 -0800

    day 1 part 2

--- 2020/src/day01.rs
+++ 2020/src/day01.rs
@@ -17,3 +17,27 @@ fn two_sum(nums: Vec<i32>, target: i32) -> (i32, i32) {
 
     panic!("No solution found");
 }
+
+pub fn part2(input: String) {
+    let nums: Vec<_> = input.lines().map(|line: &str| line.parse::<i32>().unwrap()).collect();
+    let (x, y, z) = three_sum(nums, 2020);
+    println!("{}", x * y * z);
+}
+
+fn three_sum(nums: Vec<i32>, target: i32) -> (i32, i32, i32) {
+    let available: HashSet<_> = nums.iter().cloned().collect();
+    assert_eq!(nums.len(), available.len(), "Assumption: there are no duplicates");
+    for (xi, x) in nums.iter().cloned().enumerate() {
+    for y in nums[xi..].iter().cloned() {
+        let z = target - (x + y);
+        if x == z || y == z {
+            continue;
+        }
+        if available.contains(&z) {
+            return (x, y, z)
+        }
+    }
+    }
+
+    panic!("No solution found");
+}
